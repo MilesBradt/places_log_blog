c43079c2662e3888283698af6cbcb92954d2da2e
Student <example@example.com>
2018-08-20 10:03:21 -0700

    add constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+function Place(name, location, date, job, landmark) {
+  this.name = name;
+  this.location = location;
+  this.date = date;
+  this.job = job;
+  this.landmark = landmark;
+}
+
+var antarctica = new Place("Antarctica", "Go far South until it's really cold...then keep going, now are you going North? Stop. You're there", "Last yesterday", "Penguin Erector", "Toppled over penguins");
+
+var miami = new Place ("Miami, Alaska", "The farthest north Alaska goes before Santa territory", "2001-1989", "Time travel tester", "Not palm trees");
+
+var dakota = new Place("Harpoonelk, North Dakota", "Lovely town that hardly exists", "1802", "No job but had to be rescued after time traveling accident, lovely place though", "Nice try");
+
+var moonbase = new Place ("Jimmy Dean Station", "The middle of the moon's left eye", "2077-2102", "Smuggler and gerneral layabout", "White flag on stick next to old footprints");
+
+
+
 $(document).ready(function() {
 
 });
