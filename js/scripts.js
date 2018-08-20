6a878946fdd40f467bfc324317a69f7a90f14dc1
Student <example@example.com>
2018-08-20 10:19:32 -0700

    add images to places

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,16 @@ function Place(name, location, date, job, landmark) {
   this.date = date;
   this.job = job;
   this.landmark = landmark;
+  this.image = image;
 }
 
-var antarctica = new Place("Antarctica", "Go far South until it's really cold...then keep going, now are you going North? Stop. You're there", "Last yesterday", "Penguin Erector", "Toppled over penguins");
+var antarctica = new Place("Antarctica", "Go far South until it's really cold...then keep going, now are you going North? Stop. You're there", "Last yesterday", "Penguin Erector", "Toppled over penguins", "img/antarctica.jpg");
 
-var miami = new Place ("Miami, Alaska", "The farthest north Alaska goes before Santa territory", "2001-1989", "Time travel tester", "Not palm trees");
+var miami = new Place ("Miami, Alaska", "The farthest north Alaska goes before Santa territory", "2001-1989", "Time travel tester", "Not palm trees", "img/alaska.jpg");
 
-var dakota = new Place("Harpoonelk, North Dakota", "Lovely town that hardly exists", "1802", "No job but had to be rescued after time traveling accident, lovely place though", "Nice try");
+var dakota = new Place("Harpoonelk, North Dakota", "Lovely town that hardly exists", "1802", "No job but had to be rescued after time traveling accident, lovely place though", "Nice try; trick question", "img/dakota.jpg");
 
-var moonbase = new Place ("Jimmy Dean Station", "The middle of the moon's left eye", "2077-2102", "Smuggler and gerneral layabout", "White flag on stick next to old footprints");
+var moonbase = new Place ("Jimmy Dean Station", "The middle of the moon's left eye", "2077-2102", "Smuggler and gerneral layabout", "White flag on stick next to old footprints", "img/moon.jpg");
 
 
 
